624c36c70f2b16e84c30f92088603aa696f299ea
Bailey Mejia <baileymejia28@gmail.com>
2022-12-16 23:52:08 -0800

    Added code into server.js file to add modules and get the server running.

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-//TODO Create a CMS-style blog site
+//Added  modules
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers')
+const sequelize = require('./config/connection');
 
-//require the path
-//require express
-//require session
-//require exphbs express handlebars
-//require routes
-//require helpers
-//require sequelize
 
-//require sequelizeStire
+//Need access to the module sql session store 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//intialize expressiom
-//declare the port and use env
+const app = express();
 
-//create the session object to use 
+const PORT = process.env.PORT || 3001;
 
-//allow the program to use all the modules 
+const hbs = exphbs.create({helpers});
+
+
+//Create a session cookie so a session cannot be hijacked
+const sess = {
+    Store: new SequelizeStore({
+        db: sequelize
+    })
+};
+//Use all the added modules for the server
+app.use(session(sess));
+//render webpages to the client side
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+})
