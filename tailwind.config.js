ce16a4611a85e5051fe88420442a5ddd972073f4
Bailey Mejia <baileymejia28@gmail.com>
2023-01-03 20:11:24 -0800

    Included Tailwind css for the styling option for the assignment.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        100: '#f3f4f6',
+      }
+    },
+  },
+  plugins: [],
+}
